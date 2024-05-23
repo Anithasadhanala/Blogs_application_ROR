4497d3adb59b6f8bc08a2882b396cd9db4f4d5ae
anithasadhanala <20mh1a4248@acoe.edu.in>
2024-05-23 14:18:56 +0530

    users Authentication added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,33 +26,32 @@ class UsersController < ApplicationController
     end
   
 
-    private
     def user_params
       params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
   
 
-  def login
-    user = User.find_by(email: params[:email])
+    def login
+      user = User.find_by(email: params[:email])
 
-    if user && user.authenticate(params[:password])
-     
-      session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully"
-      puts("++++++++++++++++++++++++++")
-    else
-    
-      flash.now[:alert] = "Invalid email or password"
-      render :new
+      if user && user.authenticate(params[:password])
+      
+        session[:user_id] = user.id
+        redirect_to root_path, notice: "Logged in successfully"
+        puts("++++++++++++++++++++++++++")
+      else
+      
+        flash.now[:alert] = "Invalid email or password"
+        render :new
+      end
     end
-  end
 
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out successfully"
-  end
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_path, notice: "Logged out successfully"
+    end
 
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   
   resources :blogs
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+ 
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/login", to: "users#signin"
@@ -13,6 +10,8 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  delete '/signup', to: 'users#destroy'
 
 
   root "home#index"
